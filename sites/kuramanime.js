4d5c7c77a7a77ac8e43460a7a4d9ed52e0ff0da5
deathdies <30880522+deathdies@users.noreply.github.com>
2024-09-16 22:30:00 +0800

    Update kuramanime.js

--- sites/kuramanime.js
+++ sites/kuramanime.js
@@ -111,7 +111,7 @@ router.get("/completed/:order_by/:page", Completed);
 //OrderBy Try => updated, most_viewed, popular, latest, oldest, ascending, descending
 const Movies = async (req, res) => {
   const options = {
-    url: `${baseURL}/quick/movie?order_by=${req.params.order_by}&page=${req.params.page}`;
+    url: `${baseURL}/quick/movie?order_by=${req.params.order_by}&page=${req.params.page}`,
     headers: {
       "User-Agent":
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36"
